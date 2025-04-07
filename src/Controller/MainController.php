7a560cbe0aa1c6ea321c85deca9f801ff097bf82
AlexD004 <dupre.alexandre04@gmail.com>
2025-04-07 18:24:25 +0200

    add views and routes for auth : register, log, dashboard

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,12 +5,13 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Core\Security;
+//use Symfony\Component\Security\Core\Security;
 
 final class MainController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function home(Security $security): Response
+    //public function home(Security $security): Response
+    public function home(): Response
     {
        /* $user = $security->getUser();
 
@@ -21,6 +22,24 @@ final class MainController extends AbstractController
         }*/
 
         return $this->render('home.html.twig');
+    }
+
+    #[Route('/register', name: 'register')]
+    public function register(): Response
+    {
+        return $this->render('auth/register.html.twig');
+    }
+
+    #[Route('/login', name: 'login')]
+    public function login(): Response
+    {
+        return $this->render('auth/login.html.twig');
+    }
+
+    #[Route('/dashboard', name: 'dashboard')]
+    public function dashboard(): Response
+    {
+        return $this->render('dashboard.html.twig');
     }
 
     #[Route('/{slug}', name: 'not_found', requirements: ['slug' => '.*'])]
